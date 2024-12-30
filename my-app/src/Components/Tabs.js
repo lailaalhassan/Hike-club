ac006011a1446582bbb1a685cfe0c656b387d9b9
Laila Alhassan <l.alhassan@wlv.ac.uk>
2024-12-30 20:47:10 +0000

    search bar included

--- my-app/src/Components/Tabs.js
+++ my-app/src/Components/Tabs.js
@@ -19,10 +19,22 @@ function Tabs() {
 
   return (
     <div className="container">
-      <NavBar 
-        selectedTab={selectedTab}
-        setSelectedTab={setSelectedTab}
-      />
+      <div className="d-flex justify-content-between align-items-center">
+        {/* Navigation Bar */}
+        <NavBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+        {/* Search Bar */}
+        <form className="d-flex ms-3">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
       {/* This is the main content of the page */}
       {TABS[selectedTab]}
     </div>
